2a5ad20f60f984668cd581c39741374cbae757cd
sviddo <snisarvlad04@gmail.com>
2022-06-02 00:09:58 +0300

    add posts to home page

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Vlad Snisar',
+        'title': 'My first flask blog page',
+        'content': 'This is my first flask blog page, here you can view posts made by other users, \
+            create your own ones, register, authenticate, change profile photo and do other cool stuff. Feel free to explore.',
+        'date_posted': 'june 01, 2022'
+    },
+    {
+        'author': 'Guido van Rossum',
+        'title': 'Python programming language',
+        'content': 'I\'m a father of Python programming language. \
+            Van Rossum thought he needed a name that was short, unique, \
+                and slightly mysterious, so he decided to call the language Python.',
+        'date_posted': 'June 01, 2022'
+    }
+]
+
 @app.route("/home")
 @app.route("/")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
