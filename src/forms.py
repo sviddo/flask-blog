675aa3c485fe00ab8b8ef5d8339e55ef45315296
sviddo <snisarvlad04@gmail.com>
2022-06-09 18:48:19 +0300

    implement ability to update profile info
    - users can change their username and/or email
    - add default profile image for new users which in future can be changed

--- src/forms.py
+++ src/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from src.models import User
@@ -44,3 +45,27 @@ class LoginForm(FlaskForm):
     remember = BooleanField("Remember me")
     
     submit = SubmitField("Log in")
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username",
+                    validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField("Email",
+                    validators=[DataRequired(), Email()])
+
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+
+            if user:
+                raise ValidationError("This username is taken. Please, choose another one")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+
+            if user:
+                raise ValidationError("This email is taken. Please, choose another one")
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from src import app, db, bcrypt
-from src.forms import LoginForm, RegistrationForm
+from src.forms import LoginForm, RegistrationForm, UpdateAccountForm
 from src.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -72,7 +72,19 @@ def log_out():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
+
+    return render_template('account.html', title='Account', form=form, image_file=image_file)
